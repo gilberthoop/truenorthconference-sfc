9d15c0bd1a1698b30d490840b7de88210335ee81
William Go <gilberthoop@Williams-MacBook-Pro.local>
2022-03-27 10:15:00 -0700

    Initialize Users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('region');
         });
     }
 
