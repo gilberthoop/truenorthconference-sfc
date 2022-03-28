5782522ff888ea5df4af95f8dbb9071c56140833
William Gilbert Go <williamgilbertgo@gmail.com>
2022-03-27 19:11:00 -0700

    Add images for background and logos (#4)
    Co-authored-by: William Go <gilberthoop@Williams-MacBook-Pro.local>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>True North Conference | SFC Canada</title>
+    <link rel="icon" type="image/x-icon" href="/images/tnclogo-blackred.svg">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
