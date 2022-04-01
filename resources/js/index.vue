f624a0cac56949fd4e48ee39176d812747de7de3
William Go <gilberthoop@Williams-MacBook-Pro.local>
2022-04-01 00:13:20 -0700

    Migrate MVP components into a single Home component

--- resources/js/index.vue
+++ resources/js/index.vue
@@ -1,47 +1,12 @@
 <template>
   <v-app class="app">
-    <div class="d-flex flex-column">
-      <div>
-        <AppToolBar
-        v-if="!noToolBarMenu"
-        />
-      </div>
-      <Banner
-        v-if="!noToolBarMenu"
-      />
-    </div>
-
-    <Introduction />
-
-    <v-spacer />
-
-    <AppFooter />
-  
     <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import Banner from './components/header/Banner'
-import AppToolBar from './components/header/AppToolBar'
-import AppFooter from './components/footer/AppFooter'
-import Introduction from './components/mvp/Introduction'
-
 export default {
-  name: 'Index',
-
-  components: {
-    Banner,
-    AppToolBar,
-    AppFooter,
-    Introduction
-  },
-
-  computed: {
-    noToolBarMenu () {
-      return this.$route.name === 'register' || this.$route.name === 'login' || this.$route.name === 'participants'
-    },
-  }
+  name: 'Index'
 }
 </script>
 
