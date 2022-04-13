0a9c1b34398e3edee4bc7b28ace837a43055530b
William Gilbert Go <williamgilbertgo@gmail.com>
2022-04-12 21:21:52 -0700

    Build the Home Portal (#28)

---
+++ resources/js/pages/portal/PortalHome.vue
@@ -0,0 +1,300 @@
+<template>
+  <v-app>
+    <div class="portal-home my-12 mx-6 mx-sm-9 mx-md-16">
+      <div class="portal-home__title">
+        Welcome to TNC 2022
+      </div>
+
+      <div class="portal-home__cards d-flex justify-space-between flex-column flex-md-row">
+          <v-card
+            class="portal-home__card-1 pa-6 pa-sm-10 mt-6 mr-md-3"
+            color="#EEF4FF"
+            elevation="0"
+          >
+            <div class="portal-home__event-heads my-3">
+              <v-img
+                :src="eventHeads"
+                class="mb-3"
+                height="40"
+                width="68"
+              />
+              Message from event heads
+            </div>
+
+            <div class="portal-home__card-1--title">
+              We're glad you're here!
+            </div>
+
+            <div class="portal-home__card-1--subtitle my-4">
+              Thank you for registering for this year’s True North Conference.
+              We are eagerly preparing for our upcoming weekend. Make sure you tune in on May 27-28, 2022.
+              <br/> <br/>
+              This year’s theme is In Christ.
+              As we prepare for conference, we invite you to meditate on our anchor verse:
+            </div>
+
+            <div class="bible-verse d-flex align-center my-8">
+              <v-divider
+                class="bible-verse__divider"
+                vertical
+              />
+              <div class="ml-6">
+                <v-icon
+                  medium
+                  color="#444CE7"
+                  class="mb-4"
+                >
+                  auto_stories
+                </v-icon>
+                <br/>
+                “If then my people, upon whom my name has been pronounced, humble themselves and pray,
+                and seek my face and turn from their evil ways, I will hear them from heaven and pardon their sins and heal their land.”
+                <br/><br/>
+                (2 Chronicles 7:14 NRSCVE)
+              </div>
+            </div>
+          </v-card>
+
+          <v-card
+            class="portal-home__card-2 pa-8 mt-6 ml-md-3"
+            color="#F2F4F7"
+            elevation="0"
+            height="100%"
+          >
+            <div class="portal-home__card-2--title mb-6">
+              Key reminders
+            </div>
+            <div class="portal-home__card-2--subtitle">
+              <div
+                v-for="(reminder, index) in reminders"
+                :key="index"
+                class="mt-4 d-flex align-start"
+              >
+                <v-icon
+                  medium
+                  color="#444CE7"
+                >
+                  task_alt
+                </v-icon>
+                <div class="ml-2">
+                  {{ reminder }}
+                </div>
+              </div>
+            </div>
+          </v-card>
+      </div>
+
+      <div class="portal-home__schedule my-16">
+        <div class="portal-home__schedule--title my-4">
+          Conference Schedule
+        </div>
+        <div class="portal-home__schedule--timezone">
+          <div class="portal-home__schedule--timezone-label">
+            Select your timezone
+          </div>
+          <div class="portal-home__schedule--timezone-select">
+            <v-select
+              class="my-4"
+              :items="['PST']"
+              filled
+              background-color="#EEF4FF"
+              color="#3538CD"
+            />
+          </div>
+        </div>
+
+        <v-card
+          class="portal-home__schedule--card pa-16"
+          max-width="100%"
+          max-height="100%"
+          color="#F5F8FF"
+          elevation="0"
+        >
+          <v-img
+            :src="starIcon"
+            class="mb-3 mx-auto"
+            height="20"
+            width="20"
+          />
+          <div class="portal-home__schedule--card-title my-2">
+            No schedule yet
+          </div>
+          <div class="portal-home__schedule--card-subtitle">
+            The final schedule will appear here when it’s available
+          </div>
+        </v-card>
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'PortalHome',
+
+  data () {
+    return {
+      starIcon: '/images/star-icon-purple.png',
+      eventHeads: '/images/event-heads.png',
+      reminders: [
+        'Invite your friends to join!',
+        'Encourage your chapter to compete in competitions'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.portal-home {
+  font-family: "Outfit", sans-serif;
+  color: black;
+
+  &__title {
+    font-size: 72px;
+    font-weight: 700;
+    line-height: 90px;
+
+    @media only screen and (max-width: 960px) {
+      font-size: 62px;
+    }
+
+    @media only screen and (max-width: 600px) {
+      font-size: 36px;
+      line-height: 44px;
+    }
+  }
+
+  &__event-heads {
+    color: #6172F3;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 500px;
+  }
+
+  &__card-1 {
+    max-width: 832px;
+
+    @media only screen and (max-width: 960px) {
+      max-width: 100%;
+    }
+
+    &--title {
+      font-size: 36px;
+      line-height: 44px;
+      font-weight: 500px;
+
+      @media only screen and (max-width: 600px) {
+        font-size: 30px;
+        line-height: 38px;
+      }
+    }
+
+    &--subtitle {
+      font-size: 20px;
+      line-height: 30px;
+
+      @media only screen and (max-width: 600px) {
+        font-size: 16px;
+        line-height: 24px;
+      }
+    }
+  }
+
+  &__card-2 {
+    max-width: 100%;
+
+    &--title {
+      font-size: 30px;
+      line-height: 38px;
+      font-weight: 500px;
+
+      @media only screen and (max-width: 600px) {
+        font-size: 24px;
+        line-height: 32px;
+      }
+    }
+
+    &--subtitle {
+      font-size: 20px;
+      line-height: 30px;
+
+      @media only screen and (max-width: 600px) {
+        font-size: 16px;
+        line-height: 24px;
+      }
+    }
+  }
+
+  &__schedule {
+    max-width: 100%;
+
+    &--title {
+      font-size: 48px;
+      font-weight: 700;
+      line-height: 60px;
+
+      @media only screen and (max-width: 600px) {
+        font-size: 30px;
+        line-height: 38px;
+      }
+    }
+
+    &--timezone {
+      max-width: 300px;
+
+      &-label {
+        font-size: 24px;
+        line-height: 32px;
+      }
+
+      &-select {
+        max-width: 200px;
+        ::v-deep .theme--light.v-select .v-select__selections {
+          color: #3538CD !important;
+        }
+      }
+    }
+
+    &--card {
+      text-align: center;
+      font-size: 20px;
+      line-height: 30px;
+
+      @media only screen and (max-width: 600px) {
+        font-size: 16px;
+        line-height: 24px;
+      }
+
+      &-title {
+        font-weight: 700;
+      }
+    }
+  }
+}
+
+.bible-verse {
+  color: #444CE7;
+  font-size: 16px;
+  line-height: 24px;
+  max-width: 80%;
+  height: 200px;
+
+  @media only screen and (max-width: 960px) {
+    max-width: 90%;
+    height: 100%;
+  }
+
+  @media only screen and (max-width: 600px) {
+    max-width: 100%;
+  }
+
+  &__divider {
+    height: auto;
+    border: 3px solid #444CE7;
+  }
+}
+::v-deep .app {
+  background-color: #fff !important;
+}
+</style>
